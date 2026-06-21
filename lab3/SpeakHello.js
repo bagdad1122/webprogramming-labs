5b6c935b623ede1098de3f416a30f9021d64671c
bagdad1122 <bohdanchermak1@gmail.com>
2026-06-21 15:56:07 +0300

    Update SpeakHello.js

--- lab3/SpeakHello.js
+++ lab3/SpeakHello.js
@@ -1,1 +1,16 @@
+(function (window) {
+  // Створюємо об'єкт, який буде доступний ззовні
+  var helloSpeaker = {};
+  
+  // Ця змінна залишається локальною для цієї IIFE
+  var speakWord = "Hello";
 
+  // Додаємо метод до об'єкта
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // Експортуємо об'єкт у глобальну область видимості
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
